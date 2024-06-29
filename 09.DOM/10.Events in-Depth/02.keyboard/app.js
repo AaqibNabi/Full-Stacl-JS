d8190f03103c235ed9806981e31a4a9415b64524
AaqibNabi <aaqib.shaavir@gmail.com>
2024-06-29 16:18:11 +0530

    commit

--- 09.DOM/10.Events in-Depth/02.keyboard/app.js
+++ 09.DOM/10.Events in-Depth/02.keyboard/app.js
@@ -1,41 +1,16 @@
-let one = document.querySelector(".onclick");
-let two = document.querySelector(".oncontextmenu");
-let three = document.querySelector(".ondblclick");
-let four = document.querySelector(".onmousedown");
-let five = document.querySelector(".onmouseenter");
-let six = document.querySelector(".onmouseleave");
-let seven = document.querySelector(".onmousemove");
-let eight = document.querySelector(".onmouseout");
-let nine = document.querySelector(".onmouseover");
-let ten = document.querySelector(".onmouseup");
-
-one.addEventListener("click", () => {
+let one = document.querySelector(".onkeydown");
+let two = document.querySelector(".onkeypress");
+let three = document.querySelector(".onkeyup");
+let paragraph = document.querySelector(".paragraph");
+one.addEventListener("keydown", () => {
   one.classList.toggle("red");
+  paragraph.innerHTML = "You pressed a key inside the input field";
 });
-two.addEventListener("contextmenu", () => {
-  two.classList.toggle("voilet");
+two.addEventListener("keypress", () => {
+  two.classList.toggle("blue");
+  alert("You pressed a key inside the input field");
 });
-three.addEventListener("dblclick", () => {
-  three.classList.toggle("indigo");
-});
-four.addEventListener("mousedown", () => {
-  four.classList.toggle("blue");
-});
-five.addEventListener("mouseenter", () => {
-  five.classList.toggle("green");
-});
-six.addEventListener("mouseleave", () => {
-  six.classList.toggle("yellow");
-});
-seven.addEventListener("mousemove", () => {
-  seven.classList.toggle("orange");
-});
-eight.addEventListener("mouseout", () => {
-  eight.classList.toggle("teal");
-});
-nine.addEventListener("mouseover", () => {
-  nine.classList.toggle("aqua");
-});
-ten.addEventListener("mouseup", () => {
-  ten.classList.toggle("gold");
+three.addEventListener("keyup", () => {
+  three.classList.toggle("green");
+  three.value = three.value.toUpperCase();
 });
