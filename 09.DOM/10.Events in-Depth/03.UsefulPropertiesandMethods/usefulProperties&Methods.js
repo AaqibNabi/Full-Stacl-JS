d09f97c2d43230985e2d2e60a8f6ad4c140996cf
AaqibNabi <aaqib.shaavir@gmail.com>
2024-06-30 15:00:51 +0530

    commit

---
+++ 09.DOM/10.Events in-Depth/03.UsefulPropertiesandMethods/usefulProperties&Methods.js
@@ -0,0 +1,9 @@
+// USEFULL PROPERTIES AND METHODS
+const input = document.querySelector("input");
+input.addEventListener("keypress", (e) => {
+  //   console.log(e.charCode);
+  //   console.log(e.code);
+  //   console.log(e.ctrlKey);
+  //   console.log(e.key);
+  console.log(e.shiftKey);
+});
