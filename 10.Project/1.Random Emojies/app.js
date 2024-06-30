214695b62c1ca63845391f9cdad396a61589cf58
AaqibNabi <aaqib.shaavir@gmail.com>
2024-06-30 22:04:23 +0530

    commit

---
+++ 10.Project/1.Random Emojies/app.js
@@ -0,0 +1,22 @@
+const emoji = document.querySelector("#emoji");
+const emojis = [
+  "😀",
+  "😊",
+  "😅",
+  "😍",
+  "😘",
+  "😁",
+  "😎",
+  "😶‍🌫️",
+  "🥱",
+  "🤞",
+  "🥴",
+  "😗",
+  "❤️",
+  "🫥",
+  "😏",
+  "🤐",
+];
+emoji.addEventListener("mouseover", () => {
+  emoji.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
